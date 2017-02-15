b30b8c0f8f51aa33b48db559b201009905c89448
Martijn Gorree <mgorree@gmail.com>
2017-02-15 11:32:38 +0100

    Changed default to cache result for 4.2 minutes

--- settings.default.py
+++ settings.default.py
@@ -4,7 +4,8 @@ PROXY_HOST = "localhost"
 PROXY_PORT = 8080
 
 MEMCACHED_SERVERS = ["127.0.0.1:11211"]
-MEMCACHED_TTL = 360
+# Cache result for 4.2 minutes (252 seconds)
+MEMCACHED_TTL = 252
 
 # C42 Api token
 C42_TOKEN = ""
